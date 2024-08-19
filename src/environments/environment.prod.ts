8bed5c12b47fc6588b00be6566561d1d15421c02
Felipe Reis <felipereiswork@gmail.com>
2024-08-19 14:33:00 -0300

    env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDbjOuVsaxhRSk5K0BnXOcBkUwKfIzs73U",
+    authDomain: "hotwheels-inventory-28304.firebaseapp.com",
+    projectId: "hotwheels-inventory-28304",
+    storageBucket: "hotwheels-inventory-28304.appspot.com",
+    messagingSenderId: "88635457812",
+    appId: "1:88635457812:web:b49edb96f4bf5b9b12c201",
+    measurementId: "G-V65JSPB0NQ"
+  }
 };
